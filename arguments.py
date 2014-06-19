9fdedd27fe9f3e81a6829acef31fdc08040aa99b
gauthamj84 <gautham.j@gmail.com>
2014-06-19 06:53:03 +0530

    Test checkin of file to repo

---
+++ arguments.py
@@ -0,0 +1,7 @@
+import sys
+
+print "Welcome to Python"
+print "These are the arguments "
+print '1st : {}'.format(sys.argv[0])
+print '2nd : {}'.format(sys.argv[1])
+print '3rd : {}'.format(sys.argv[2])
